ec9b4a3bb0fb2e5765036c229e7f144e9485127d
nmemmert <67961292+nmemmert@users.noreply.github.com>
2025-12-18 14:32:26 -0500

    Add home page collection to Tina for editing content/_index.md

--- .tina/config.ts
+++ .tina/config.ts
@@ -65,6 +65,17 @@ export default {
           { type: "rich-text", name: "body", label: "Body", isBody: true },
         ],
       },
+      {
+        name: "home",
+        label: "Home Page",
+        path: "content",
+        match: { include: "_index*" },
+        ui: { allowedActions: { create: false, delete: false } },
+        fields: [
+          { type: "string", name: "title", label: "Title", isTitle: true, required: true },
+          { type: "rich-text", name: "body", label: "Body", isBody: true },
+        ],
+      },
     ],
   },
   media: {
